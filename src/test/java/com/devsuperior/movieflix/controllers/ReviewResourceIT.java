c7b4c71de92265a12c46d5a960de3f6382badf20
rsbettini <robson.bettini@hotmail.com>
2022-03-01 21:46:16 -0300

    Tests ok

--- src/test/java/com/devsuperior/movieflix/controllers/ReviewResourceIT.java
+++ src/test/java/com/devsuperior/movieflix/controllers/ReviewResourceIT.java
@@ -1,4 +1,4 @@
-package com.devsuperior.movieflix.controllers;
+ package com.devsuperior.movieflix.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
